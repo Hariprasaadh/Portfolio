97cf813db6d9fc7ceabb3f93c3542a0a0ce42e27
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-02-23 16:17:37 +0530

    resume

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import resume from "../assets/Personal_Resume.pdf";
+import resume from "../assets/resume.pdf";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 
 export const Banner = () => {
