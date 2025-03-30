2e49f5f01cffbf92db6c01b84e1ad98beba4bb5b
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-03-30 16:42:15 +0530

    update skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -82,14 +82,19 @@ export const skills = [
             "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
         },
         {
-          name: "React Js",
+          name: "React",
           image:
             "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
         },
         {
-          name: "Bootstrap",
+          name: "Tailwind CSS",
           image:
-            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+            "https://icon.icepanel.io/Technology/svg/Tailwind-CSS.svg",
+        },
+        {
+          name: "Next.js",
+          image:
+            "https://img.icons8.com/fluent-systems-filled/512/FFFFFF/nextjs.png",
         },
       ],
     },
@@ -115,9 +120,9 @@ export const skills = [
             "https://icon.icepanel.io/Technology/svg/FastAPI.svg",
         },
         {
-          name: "Streamlit",
+          name: "Qdrant",
           image:
-            "https://icon.icepanel.io/Technology/svg/Streamlit.svg",
+            "https://avatars.githubusercontent.com/u/153958985?s=280&v=4",
         }
       ],
     },
